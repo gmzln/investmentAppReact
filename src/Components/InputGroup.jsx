ba158df56e66406ac9861954b3a32b667d84c819
gmzln <gamze_ya@outlook.de>
2025-04-28 10:21:18 +0200

    refactor input components

--- src/Components/InputGroup.jsx
+++ src/Components/InputGroup.jsx
@@ -1,10 +1,34 @@
-// import UserInput from "./UserInput";
+const inputFields = [
+  "initial investment",
+  "annual investment",
+  "expected return",
+  "duration",
+];
 
-// function InputGroup() {
-//   return (
-//     <div className="input-group">
-//       <UserInput />
-//     </div>
-//   );
-// }
-// export default InputGroup;
+function InputGroup() {
+  return (
+    <div>
+      <div className="input-group">
+        <div>
+          <label>{inputFields[0]}</label>
+          <input type="number" />
+        </div>
+        <div>
+          <label>{inputFields[1]}</label>
+          <input type="number" />
+        </div>
+      </div>
+      <div className="input-group">
+        <div>
+          <label>{inputFields[2]}</label>
+          <input type="number" />
+        </div>
+        <div>
+          <label>{inputFields[3]}</label>
+          <input type="number" />
+        </div>
+      </div>
+    </div>
+  );
+}
+export default InputGroup;
--- src/Components/UserInput.jsx
+++ src/Components/UserInput.jsx
@@ -1,33 +1,9 @@
-function UserInput() {
-  const inputFields = [
-    "initial investment",
-    "annual investment",
-    "expected return",
-    "duration",
-  ];
+import InputGroup from "./InputGroup";
 
+function UserInput() {
   return (
     <div id="user-input">
-      <div className="input-group">
-        <div>
-          <label>initial investment</label>
-          <input type="number" />
-        </div>
-        <div>
-          <label>annual investment</label>
-          <input type="number" />
-        </div>
-      </div>
-      <div className="input-group">
-        <div>
-          <label>expected return</label>
-          <input type="number" />
-        </div>
-        <div>
-          <label>duration</label>
-          <input type="number" />
-        </div>
-      </div>
+      <InputGroup />
     </div>
   );
 }
